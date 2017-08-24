2effa490d9a15939fe82fc304a7c6f0a9e8deb0d
Lillian González Albino <lillian.gonzalez2@upr.edu>
2017-08-24 16:57:14 -0400

    Update numberExercises.scm

--- numberExercises.scm
+++ numberExercises.scm
@@ -40,21 +40,19 @@
 			((null? m) 0)
 			(else (+ n (* n (sub1 m)))))))
 
-listas del mismo tamaño
+;;same sized lists
 (define tup+
 	(lambda (tup1 tup2)
 		(cond
 			((and (null? tup1) (null? tup2))(quote()))
 			(else (cons (+ (car tup1)(car tup2))(tup+ (cdr tup1)(cdr tup2)))))))
 
-*****
 (define tup+
 	(lambda (tup1 tup2)
 		(cond
 			((null? tup1) tup2)
 			((null? tup2) tup1)
 			(else (cons (+ (car tup1)(car tup2))(tup+ (cdr tup1)(cdr tup2)))))))
-*****
 
 (define >
 	(lambda (n m)
